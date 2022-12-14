e6459d0737a44b2c4c2d8b4471b5cd1c632ef256
German Schmitz <germanschmitz123@gmail.com>
2022-12-14 14:28:26 -0300

    Agregado de mock tasks

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,21 @@
+import { Task } from './task';
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Terminar modulo 3',
+    day: 'Agosto 5 a las 12:00',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Hacer compras',
+    day: 'Agosto 5 a las 17:00',
+    reminder: false,
+  },
+  {
+    id: 3,
+    text: 'Investigar bootstrap',
+    day: 'Agosto 6 a las 10:00',
+    reminder: true,
+  },
+];
