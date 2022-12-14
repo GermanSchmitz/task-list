3874c03d1846d656964b231ca4887de0dd85d2ba
German Schmitz <germanschmitz123@gmail.com>
2022-12-14 15:26:37 -0300

    componente lista items

--- src/app/components/tasks-item/tasks-item.component.ts
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -1,12 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-tasks';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-tasks-item',
   templateUrl: './tasks-item.component.html',
   styleUrls: ['./tasks-item.component.css']
 })
 export class TasksItemComponent implements OnInit{
+  
+  faTimes = faTimes
   constructor(){}
   ngOnInit(): void {
     
