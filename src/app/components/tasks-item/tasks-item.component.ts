cf6b8d1497bf218be909e622f40b7ebc76a5f089
German Schmitz <germanschmitz123@gmail.com>
2022-12-16 14:19:09 -0300

    agregado de reminder al boton con el servicio + front

--- src/app/components/tasks-item/tasks-item.component.ts
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -1,20 +1,27 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-tasks';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-tasks-item',
   templateUrl: './tasks-item.component.html',
-  styleUrls: ['./tasks-item.component.css']
+  styleUrls: ['./tasks-item.component.css'],
 })
-export class TasksItemComponent implements OnInit{
+export class TasksItemComponent implements OnInit {
+  faTimes = faTimes;
+  @Input() task: Task = TASKS[0];
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter()
   
-  faTimes = faTimes
-  constructor(){}
-  ngOnInit(): void {
-    
+
+  constructor() {}
+  ngOnInit(): void {}
+
+  onDelete(taskParam: Task) {
+    this.onDeleteTask.emit(taskParam)
   }
 
-  @Input() task: Task = TASKS[0] 
-
+  onToggle(task: Task){
+    this.onToggleReminder.emit(task)
+  }
 }
